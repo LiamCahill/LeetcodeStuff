5b28823213aacd954928cbe1798878bfc63d337f
Liam <42883622+LiamCahill@users.noreply.github.com>
2024-01-24 20:21:48 -0500

    Initial commit.

---
+++ src/main/java/com/cahill/leetcodestuff/LeetcodeStuffApplication.java
@@ -0,0 +1,13 @@
+package com.cahill.leetcodestuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeStuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeStuffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cahill/leetcodestuff/LeetcodeStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cahill.leetcodestuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeStuffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
